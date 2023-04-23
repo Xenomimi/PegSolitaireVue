9a2f6230ed8ceb190c43e4dd32e5f2d920357255
Xenomimi <michal.biesiadecki4@gmail.com>
2023-04-23 15:11:20 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import gra from './components/gra.vue'
+import strona2 from './components/strona2.vue'
+</script>
+
+<template>
+<gra/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/gra.vue
@@ -0,0 +1,80 @@
+<script setup>
+</script>
+
+<template>
+<div class="container">
+  <div draggable="true" class="box">X</div>
+  <div draggable="true" class="box">X</div>
+  <div draggable="true" class="box">o</div>
+  <div draggable="true" class="box">o</div>
+  <div draggable="true" class="box">o</div>
+  <div draggable="true" class="box">X</div>
+  <div draggable="true" class="box">X</div>
+
+  <div draggable="true" class="box">X</div>
+  <div draggable="true" class="box">X</div>
+  <div draggable="true" class="box">o</div>
+  <div draggable="true" class="box">o</div>
+  <div draggable="true" class="box">o</div>
+  <div draggable="true" class="box">X</div>
+  <div draggable="true" class="box">X</div>
+
+  <div draggable="true" class="box">o</div>
+  <div draggable="true" class="box">o</div>
+  <div draggable="true" class="box">o</div>
+  <div draggable="true" class="box">o</div>
+  <div draggable="true" class="box">o</div>
+  <div draggable="true" class="box">o</div>
+  <div draggable="true" class="box">o</div>
+
+  <div draggable="true" class="box">o</div>
+  <div draggable="true" class="box">o</div>
+  <div draggable="true" class="box">o</div>
+  <div draggable="true" class="box">o</div>
+  <div draggable="true" class="box">o</div>
+  <div draggable="true" class="box">o</div>
+  <div draggable="true" class="box">o</div>
+
+  <div draggable="true" class="box">o</div>
+  <div draggable="true" class="box">o</div>
+  <div draggable="true" class="box">o</div>
+  <div draggable="true" class="box">o</div>
+  <div draggable="true" class="box">o</div>
+  <div draggable="true" class="box">o</div>
+  <div draggable="true" class="box">o</div>
+
+  <div draggable="true" class="box">X</div>
+  <div draggable="true" class="box">X</div>
+  <div draggable="true" class="box">o</div>
+  <div draggable="true" class="box">o</div>
+  <div draggable="true" class="box">o</div>
+  <div draggable="true" class="box">X</div>
+  <div draggable="true" class="box">X</div>
+
+  <div draggable="true" class="box">X</div>
+  <div draggable="true" class="box">X</div>
+  <div draggable="true" class="box">o</div>
+  <div draggable="true" class="box">o</div>
+  <div draggable="true" class="box">o</div>
+  <div draggable="true" class="box">X</div>
+  <div draggable="true" class="box">X</div>
+</div>
+</template>
+
+<style scoped>
+.container {
+    display: grid;
+    grid-template-columns: repeat(7, 1fr);
+    justify-items: center;
+}
+
+.box {
+    border: 1px solid #666;
+    background-color: #ddd;
+    border-radius: .5em;
+    padding: 1rem;
+    cursor: move;
+    width: 25px;
+    text-align: center;
+}
+</style>
