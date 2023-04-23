e0a831a0f43509c8de5d30e372cda6f42fd28638
Xenomimi <michal.biesiadecki4@gmail.com>
2023-04-23 15:41:00 +0200

    game board grid

--- src/components/gra.vue
+++ src/components/gra.vue
@@ -2,7 +2,8 @@
 </script>
 
 <template>
-<div class="container">
+<div class="game">
+    <div class="container">
   <div draggable="true" class="box">X</div>
   <div draggable="true" class="box">X</div>
   <div draggable="true" class="box">o</div>
@@ -59,22 +60,30 @@
   <div draggable="true" class="box">X</div>
   <div draggable="true" class="box">X</div>
 </div>
+</div>
+
 </template>
 
 <style scoped>
 .container {
     display: grid;
-    grid-template-columns: repeat(7, 1fr);
+    grid-template-columns: repeat(7, 50px);
+    grid-template-rows: repeat(7, 50px);
+    align-items: center;
     justify-items: center;
+    gap: 20px;
 }
 
 .box {
+    display: flex;
+    align-items: center;
+    justify-content: center;
     border: 1px solid #666;
     background-color: #ddd;
-    border-radius: .5em;
-    padding: 1rem;
+    border-radius: 50%;
+    padding: 20px;
     cursor: move;
-    width: 25px;
-    text-align: center;
+    width: 10px;
+    height: 10px;
 }
 </style>
